0dd49faf69cbf2ec8dd75945116ace647561f78a
James <jamesmcdaniel33@hotmail.com>
2023-05-11 22:40:09 -0500

    initial commit

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+
+            int b = 4;
+
+            int sum = a + b;
+
+            int product = a * b;
+
+            int difference = a - b;
+
+            int quotient = a / b;
+
+            int remainder = a % b;
+
+            if (a == 17 && b == 4)
+            {
+                Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+
+                Console.WriteLine($"{a}*{b} is {product}, {a}-{b} is {difference}, and {a}+{b} is {sum}");
+            }
         }
     }
 }
